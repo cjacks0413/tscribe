718925ebf1d8fa28ab78714a9d13d579873f5fb8
cjacks0413 <chjackson93@gmail.com>
2013-12-03 09:12:31 -0500

    slow playback speed

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,7 @@
 $(document).ready(function(start, stop){
 	myAudio = document.getElementById('segment');
 	myAudio.addEventListener('canplaythrough', function() {
+		this.playbackRate = 0.6; 
 		var start = $(this).data('start');
 		var stop = $(this).data('stop'); 
 		if(this.currentTime < start){this.currentTime = start;}
